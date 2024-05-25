48c79428738c782ff1851498cb54b48174654ee6
Vic1109 <96169479+Vic1109@users.noreply.github.com>
2024-05-25 22:36:23 +0800

    binding port
    bind all nic

--- frontend/app.js
+++ frontend/app.js
@@ -65,6 +65,6 @@ app.post('/upload', upload.single('poster'), (req, res) => {
     });
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running at http://localhost:${port}`);
   });

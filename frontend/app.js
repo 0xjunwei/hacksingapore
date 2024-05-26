362395165b3608311278ab9fc80d8fe194952b39
Vic1109 <96169479+Vic1109@users.noreply.github.com>
2024-05-26 09:38:01 +0800

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -76,7 +76,7 @@ app.post("/upload", upload.single("poster"), (req, res) => {
   );
 });
 
-app.get("/details/:id", (req, res) => {
+app.get("/details-:id", (req, res) => {
   const listingId = req.params.id;
   let sql = "SELECT NAME, INFO, PLACE, START, END FROM EVENTS WHERE EID = ?";
   db.query(sql, [listingId], (err, results) => {
